8ac473b6601e0547ef23b0076ac5babcd2d89d2c
ManibharathiIT <124643699+ManibharathiIT@users.noreply.github.com>
2024-07-03 10:27:32 +0530

    we can convert to one currency to another currency

---
+++ main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import Calculator from './Calculator'
+import Currency_Converter from './Currency_Converter'
+import './Currency.css'
+// import Weather from './Weather'
+// import Simple_Api from './Simple_Api'
+// import User_Details from './UserDetails'
+// import App from "./App.jsx";
+// import './index.css'
+// import './QRcode.css'
+// import QR_Code from './QR_Code'
+// import Newstate from './Newstate'
+// import UserDetails from './UserDetails'
+// import './Details.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <QR_Code /> */}
+    {/* <Newstate/> */}
+    {/* <UserDetails/> */}
+    {/* <Simple_Api/> */}
+    {/* <Weather/> */}
+    {/* <Calculator/> */}
+    <Currency_Converter/>
+  </React.StrictMode>,
+)
+
